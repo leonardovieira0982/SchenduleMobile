93c60035c81a153e4298fb1c8870f92abdaa8876
Leonardo Vieira <leonardo.vieira0982@hotmail.com>
2020-07-23 11:20:39 -0300

    Add project files.

---
+++ SchenduleApp/SchenduleApp/Views/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SchenduleApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            NavigationPage.SetHasBackButton(this, true);
+            NavigationPage.SetBackButtonTitle(this, "Back");
+            InitializeComponent();
+
+          
+
+        }
+
+        private void ScrollView_Scrolled()
+        {
+
+        }
+
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Login Sucess", "Ok");
+        }
+
+        void SignUpProcedure(object sender, EventArgs e)
+        {
+            Application.Current.MainPage.Navigation.InsertPageBefore(new Register(), new Login());
+            Application.Current.MainPage = new NavigationPage(new Register()); 
+        }
+    }
+}
---
+++ SchenduleApp/SchenduleApp/Views/Register.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SchenduleApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Register : ContentPage
+    {
+        public Register()
+        {
+            
+            NavigationPage.SetHasBackButton(this, true);
+            NavigationPage.SetBackButtonTitle(this, "Back");
+
+            InitializeComponent();
+        }
+    }
+}
