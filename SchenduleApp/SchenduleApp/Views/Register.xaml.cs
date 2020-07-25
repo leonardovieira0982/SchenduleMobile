6f65e251ec3af0b8c94d3430afbef6d0c1db4e25
Leonardo Vieira <leonardo.vieira0982@hotmail.com>
2020-07-25 10:10:58 -0300

    Navigation flow and Entry tabs

--- SchenduleApp/SchenduleApp/Views/Register.xaml.cs
+++ SchenduleApp/SchenduleApp/Views/Register.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,12 +8,13 @@ namespace SchenduleApp.Views
     public partial class Register : ContentPage
     {
         public Register()
-        {
-            
-            NavigationPage.SetHasBackButton(this, true);
-            NavigationPage.SetBackButtonTitle(this, "Back");
-
+        {                        
             InitializeComponent();
+        }
+
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Login Sucess", "Ok");
         }
     }
 }
